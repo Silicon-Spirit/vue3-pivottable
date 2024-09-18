488524cb0e016fcde5ddd5995fc71f1c432ba4cf
Tim-jn <Tim.jeanmart@hotmail.com>
2024-09-18 10:27:42 +0200

    feat: update to VueJS3

--- Pivottable.js
+++ Pivottable.js
@@ -2,18 +2,18 @@ import TableRenderer from "./TableRenderer";
 import defaultProps from "./helper/defaultProps";
 import * as Vue from "vue";
 export default {
-  name: "vue-pivottable",
-  props: defaultProps.props,
-  computed: {
-    renderers() {
-      return TableRenderer[
-        this.rendererName in TableRenderer
-          ? this.rendererName
-          : Object.keys(TableRenderer)[0]
-      ];
-    },
-  },
-  render() {
-    return Vue.h(this.renderers, { ...this.$props });
-  },
+	name: "vue-pivottable",
+	props: defaultProps.props,
+	computed: {
+		renderers() {
+			return TableRenderer[
+				this.rendererName in TableRenderer
+					? this.rendererName
+					: Object.keys(TableRenderer)[0]
+			];
+		},
+	},
+	render() {
+		return Vue.h(this.renderers, { ...this.$props });
+	},
 };
