674400be9b9f59858d0d98a95b09801036bda501
Tim-jn <Tim.jeanmart@hotmail.com>
2024-10-31 12:13:24 +0100

    feat: DraggableAttribute (update filter icon)

--- DraggableAttribute.js
+++ DraggableAttribute.js
@@ -291,7 +291,24 @@ export default {
 										class: ["pvtTriangle"],
 										onClick: this.toggleFilterBox.bind(this),
 									},
-									"  ▾"
+									[
+										Vue.h(
+											"svg",
+											{
+												xmlns: "http://www.w3.org/2000/svg",
+												width: "12",
+												height: "12",
+												fill: "currentColor",
+												class: "bi bi-filter-right",
+												viewBox: "0 0 16 16",
+											},
+											[
+												Vue.h("path", {
+													d: "M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5",
+												}),
+											]
+										),
+									]
 								)
 							: undefined,
 						this.open ? this.getFilterBox() : undefined,
